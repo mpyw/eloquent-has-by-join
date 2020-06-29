9117cb076422adf48be08cb898b045dbe4eabd3a
mpyw <ryosuke_i_628@yahoo.co.jp>
2020-06-29 12:07:11 +0900

    Fix: Correct IDE helper return types

--- _laravel_ide_helper.php
+++ _laravel_ide_helper.php
@@ -8,9 +8,9 @@ namespace Illuminate\Database\Eloquent
             /**
              * Convert has() and whereHas() constraints to join() ones for BelongsTo and HasOne relations.
              *
-             * @param  string|string[]                       $relationMethod
-             * @param  callable[]|null[]                     $constraints
-             * @return \Illuminate\Database\Eloquent\Builder
+             * @param  string|string[]   $relationMethod
+             * @param  callable[]|null[] $constraints
+             * @return $this
              * @see \Mpyw\EloquentHasByJoin\HasByJoinMacro
              */
             public function hasByJoin($relationMethod, ?callable ...$constraints)
